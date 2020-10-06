a85af4e971c5e485657a7286d4700a1dca24a338
Makena Dettmann <mdettmann@ucdavis.edu>
2020-10-06 15:21:59 -0700

    new test

--- tweepyTest.py
+++ tweepyTest.py
@@ -1,5 +1,6 @@
 import json
 import tweepy
+from scholarly import scholarly
 
 with open('creds.json') as f:
     keys = json.load(f)
@@ -7,6 +8,7 @@ with open('creds.json') as f:
 auth = tweepy.OAuthHandler(keys["api_key"], keys["api_key_secret"])
 auth.set_access_token
 api = tweepy.API(auth)
-public_tweets = api.home_timeline()
-for tweet in public_tweets:
-    print(tweet.text)
+print(next(scholarly.search_author('Adam Moule')))
+# public_tweets = api.home_timeline()
+# for tweet in public_tweets:
+#     print(tweet.text)
