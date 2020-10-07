490180aa6ecdd3fd7757461fa9628774521ebebc
Makena Dettmann <mdettmann@ucdavis.edu>
2020-10-07 11:39:45 -0700

    tweepyTest gets all of adamBot's tweets

--- tweepyTest.py
+++ tweepyTest.py
@@ -6,9 +6,9 @@ with open('creds.json') as f:
     keys = json.load(f)
 
 auth = tweepy.OAuthHandler(keys["api_key"], keys["api_key_secret"])
-auth.set_access_token
+auth.set_access_token(keys["access_token"], keys["access_token_secret"])
 api = tweepy.API(auth)
 print(next(scholarly.search_author('Adam Moule')))
-# public_tweets = api.home_timeline()
-# for tweet in public_tweets:
-#     print(tweet.text)
+public_tweets = api.home_timeline()
+for tweet in public_tweets:
+    print(tweet.text)
