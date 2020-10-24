4b4b78ea851edabb900f0fa48e800b6d1a7b5603
Abegail Diaz <abdiaz@ucdavis.edu>
2020-10-23 22:04:50 -0700

    updates

--- TwitterBot.py
+++ TwitterBot.py
@@ -1,37 +1,39 @@
 import json
 import tweepy
-from scholarly import scholarly
+from scholarly import scholarly # ProxyGenerator
+
+# pg = ProxyGenerator()
+# pg.FreeProxies()
+# scholarly.use_proxy(pg)
 
 with open('creds.json') as f:
     keys = json.load(f)
-
 # authenticate to Twitter
 auth = tweepy.OAuthHandler(keys["api_key"], keys["api_key_secret"])
 auth.set_access_token(keys["access_token"], keys["access_token_secret"])
-
 # Create API object
 api = tweepy.API(auth)
-
-# Create a tweet
-# api.update_status("Tweet Test")
-# new_tweet = api.update_status()
 
 # Retrieve the author's data, fill-in, and print
 search_query = scholarly.search_author('Adam Moule')
 author = next(search_query).fill()
 # print(author.fill(sections=['basics']))
-
-# Print the titles of the author's publications
-# print([pub.bib['title'] for pub in author.publications])
+# author = next(search_query)
+# print(author)
 
 # Count number of author's publications
 titles = ([pub.bib['title'] for pub in author.publications])
 num = len(titles) - 1 
 # print(num)
 
-# Take a closer look at the __ publication
-pub = author.publications[num].fill()
-print(pub)
+# Take a closer look at the last publication
+last_pub = author.publications[num].fill()
+# print(last_pub)
+titles.reverse()
+# print(titles[0])
+#link = ([pub.bib['url'] for pub in author.publications])
 
-# # Which papers cited that publication?
-# print([citation.bib['title'] for citation in pub.citedby])
+# Tweet last paper
+# api.update_status("Check out our latest paper!" + str(titles[0]) + str(link))
+
+# print("Check out our latest paper!" + str(title[0]))
